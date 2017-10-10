1cbcaef22ab13b50783879f2e09a89b169090d58
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-10-11 00:37:56 +0530

    Update 1.You-have-Already-Been-Introduced.py

--- python/1.You-have-Already-Been-Introduced.py
+++ python/1.You-have-Already-Been-Introduced.py
@@ -1,3 +1,9 @@
+#The good news is that if you've gotten this far (that is, to the Codecademy website), you've met HTTP before! It's that little bit before the "www" in a website's address. Some browsers might hide it from you, but Codecademy's full address is:
+#
+#http://www.codecademy.com/
+#The HTTP stands for HyperText Transfer Protocol. HyperText is text with links in it (like this!) and a transfer protocol is a fancy way of saying "rules for getting something from one place to another." In this case, the rules are for transferring web pages to your browser.
+
+
 from urllib2 import Request, urlopen, URLError
 
 request = Request('http://placekitten.com/')
